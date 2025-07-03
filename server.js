cfac1a90b963a4b5d3c562031d25faaf7bb861d8
shaurya <2447shaurya48@gmail.com>
2025-07-03 07:27:38 +0530

    Fix server port configuration and remove Vercel configuration file

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ connectDB()
         throw error
     })
 
-    app.listen(process.env.PORT || 8000,()=>{
+    app.listen(process.env.PORT || 4000,()=>{
         console.log(`Server is running at port : ${port}`)
     })
 })
